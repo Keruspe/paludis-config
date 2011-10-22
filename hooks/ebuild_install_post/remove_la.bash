f1d9b307ef780a839c70b6b8b032b6da37dbb801
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2011-10-22 13:27:15 +0200

    whitelist another la ...
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- hooks/ebuild_install_post/remove_la.bash
+++ hooks/ebuild_install_post/remove_la.bash
@@ -1,3 +1,3 @@
 # vim: set sw=4 sts=4 et :
 
-find ${D} -name '*.la' ! -name libltdl.la ! -name libgnome-bluetooth-applet.la -exec rm '{}' +
+find ${D} -name '*.la' ! -name libltdl.la ! -name libgnome-bluetooth.la ! -name libgnome-bluetooth-applet.la -exec rm '{}' +
