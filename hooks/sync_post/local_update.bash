629fc22d217ad7e19eafbb0dd2938912bffac307
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-08-13 23:15:29 +0200

    autopatch for repos
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ hooks/sync_post/local_update.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+dir=/var/db/paludis/repositories/${TARGET}
+
+[[ -d ${dir} ]] || exit 0
+
+source /usr/libexec/paludis/echo_functions.bash
+
+pushd $dir &>/dev/null
+for p in *.patch; do
+   [[ -f ${p} ]] || continue
+   if ! git am ${p}; then
+       git am --abort
+       ewarn Patch failed: ${p}
+       exit 1
+   fi
+done
+popd &>/dev/null
+
+exit 0
